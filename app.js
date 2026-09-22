8a1f6c0befec8dfb54ab2fbdd9e669e96b691d31
mae14828 <migi2412@outlook.jp>
2026-09-22 23:11:38 +0900

    fix: 入力履歴の表示を小数点以下2桁に修正

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ calcButton.addEventListener("click",function(){
     caloriesResult.textContent = totalCalories.toFixed(2);
 
     const newInputItem = document.createElement("li");
-    newInputItem.textContent = `タンパク質: ${protein}g, 脂質: ${fat}g, 糖質: ${carb}g, カロリー: ${calories}kcal`;
+    newInputItem.textContent = `タンパク質: ${protein.toFixed(2)}g, 脂質: ${fat.toFixed(2)}g, 糖質: ${carb.toFixed(2)}g, カロリー: ${calories.toFixed(2)}kcal`;
     inputList.appendChild(newInputItem);
 
     proteinInput.value = "";
