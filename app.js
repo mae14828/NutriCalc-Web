0a37ba565d279c8e00c40e485dd45f7048eeea93
mae14828 <migi2412@outlook.jp>
2026-09-23 22:59:42 +0900

    feat: 塩分の合計を計算し、結果を表示する機能を追加

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const calcButton = document.getElementById("calcButton");
 const proteinInput = document.getElementById("proteinInput");
 const fatInput = document.getElementById("fatInput");
 const carbInput = document.getElementById("carbInput");
+const saltInput = document.getElementById("saltInput");
 
 const liveCalories = document.getElementById("liveCalories");
 const proteinResult = document.getElementById("proteinResult");
 const fatResult = document.getElementById("fatResult");
 const carbResult = document.getElementById("carbResult");
+const saltResult = document.getElementById("saltResult");
 const caloriesResult = document.getElementById("caloriesResult");
 
 let totalProtein = 0;
 let totalFat = 0;
 let totalCarb = 0;
+let totalSalt = 0;
 let totalCalories = 0;
 
 liveCaloriesButton.addEventListener("click",function(){
@@ -27,16 +30,19 @@ calcButton.addEventListener("click",function(){
     let protein = Number(proteinInput.value);
     let fat = Number(fatInput.value);
     let carb = Number(carbInput.value);
+    let salt = Number(saltInput.value);
     let calories = protein *4 + fat *9 + carb *4;
 
     totalProtein += protein;
     totalFat += fat;
     totalCarb += carb;
     totalCalories += calories;
+    totalSalt += salt;
 
     proteinResult.textContent = totalProtein.toFixed(2);
     fatResult.textContent = totalFat.toFixed(2);
     carbResult.textContent = totalCarb.toFixed(2);
+    saltResult.textContent = totalSalt.toFixed(2);
     caloriesResult.textContent = totalCalories.toFixed(2);
 
     const newInputItem = document.createElement("li");
